8e67ffb920fe813f42622d5c5b069ed96b608b88
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-26 11:20:34 +0000

    Placeholder text replacement

--- components/simple_footer_component_1721992814.js
+++ components/simple_footer_component_1721992814.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1721992814", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            InfiniMake: Endless Creative Possibilities
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        InfiniMake is a revolutionary app that empowers you to unleash your creativity without limits. Explore infinite possibilities, craft unique designs, and bring your wildest ideas to life. Join our community of makers and embark on an endless journey of innovation.
                     </div>
                 </div>
             </div>
